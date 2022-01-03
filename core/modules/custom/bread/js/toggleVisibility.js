f51daf0b111bea24d55f629f422cdfd2c268c03c
Bailey Lievens <baileylievens@hotmail.be>
2022-01-03 18:23:48 +0100

    Add jank requirements

---
+++ core/modules/custom/bread/js/toggleVisibility.js
@@ -0,0 +1,43 @@
+(function ($, Drupal) {
+    'use strict';
+
+    //Create behavior for form
+    Drupal.behaviors.formBehavior = {
+        attach: function (context, settings) {
+            $('.bread-form', context).once('formbehavior').each(function () {
+
+                var dessertContainer = $(".dessert-options");
+                var breadContainer = $(".bread-options");
+
+                var breadForm = $(".bread-form");
+                
+                $('input[type=radio][name=what_type]').change(function() {
+                    if (this.value == 'bread') {
+                        breadContainer.css("display", "inline");
+                        $('.bread-options select').prop('required',true);
+
+                        dessertContainer.css("display", "none");
+                    }
+                    else if (this.value == 'dessert') {
+                        dessertContainer.css("display", "inline");
+
+                        breadContainer.css("display", "none");
+                        $('.bread-options select').prop('required',false);
+                    }
+                });
+
+                breadForm.submit(function(e){
+                    var formData = breadForm.serializeArray();
+                    if(formData[0].value == "dessert"){
+                        if(formData.length < 4){
+                            alert("Select at least one type of dessert.🍦");
+                        }
+                    }
+                    e.preventDefault();
+                })
+
+            });
+        }
+      };
+
+})(jQuery, Drupal);
